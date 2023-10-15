e622e9530cae8b18c67666cb572ed8bb149b3177
lia237 <https://github.com/lia237/alx-low_level_programming.git>
2023-10-15 11:27:46 -0700

    test

--- models/base_model.py
+++ models/base_model.py
@@ -1,115 +1,25 @@
-#!/usr/bin/python3
-<<<<<<< HEAD
-"""
-Module: base.py
-"""
-import models
 import uuid
 from datetime import datetime
 
-
-class BaseModel():
-    """
-    Base class which defines all common
-    attributes/methods for other classes
-    """
-
-    def __init__(self, *args, **kwargs):
-        """
-        instatiates an object with it's
-        attributes
-        """
-        if len(kwargs) > 0:
-            for key, value in kwargs.items():
-                if key == '__class__':
-                    continue
-                if key == "created_at" or key == "updated_at":
-                    value = datetime.fromisoformat(value)
-                setattr(self, key, value)
-            return
-
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-
-        models.storage.new(self)
-
-    def __str__(self):
-        """
-        Returns the string representation
-        of the instance
-        """
-        return "[{}] ({}) {}".format(
-            type(self).__name__, self.id, self.__dict__)
-
-    def save(self):
-        """
-        updates the public instance attribute
-        updated_at with the current datetime
-        """
-        self.updated_at = datetime.now()
-        models.storage.save()
-
-    def to_dict(self):
-        """
-        returns a dictionary containing all keys/values
-        of __dict__ of the instance
-        """
-        dict = {**self.__dict__}
-        dict['__class__'] = type(self).__name__
-        dict['created_at'] = dict['created_at'].isoformat()
-        dict['updated_at'] = dict['updated_at'].isoformat()
-
-        return dict
-=======
-"""Defines the BaseModel class."""
-import models
-from uuid import uuid4
-from datetime import datetime
-
-
 class BaseModel:
-    """Represents the BaseModel of the HBnB project."""
-
     def __init__(self, *args, **kwargs):
-        """Initialize a new BaseModel.
-
-        Args:
-            *args (any): Unused.
-            **kwargs (dict): Key/value pairs of attributes.
-        """
-        tform = "%Y-%m-%dT%H:%M:%S.%f"
-        self.id = str(uuid4())
-        self.created_at = datetime.today()
-        self.updated_at = datetime.today()
-        if len(kwargs) != 0:
-            for k, v in kwargs.items():
-                if k == "created_at" or k == "updated_at":
-                    self.__dict__[k] = datetime.strptime(v, tform)
-                else:
-                    self.__dict__[k] = v
-        else:
-            models.storage.new(self)
-
-    def save(self):
-        """Update updated_at with the current datetime."""
-        self.updated_at = datetime.today()
-        models.storage.save()
-
-    def to_dict(self):
-        """Return the dictionary of the BaseModel instance.
-
-        Includes the key/value pair __class__ representing
-        the class name of the object.
-        """
-        rdict = self.__dict__.copy()
-        rdict["created_at"] = self.created_at.isoformat()
-        rdict["updated_at"] = self.updated_at.isoformat()
-        rdict["__class__"] = self.__class__.__name__
-        return rdict
+        self.id = str(uuid.uuid4())
+        self.created_at = self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    setattr(self, key, value)
 
     def __str__(self):
-        """Return the print/str representation of the BaseModel instance."""
-        clname = self.__class__.__name__
-        return "[{}] ({}) {}".format(clname, self.id, self.__dict__)
->>>>>>> 03c7647ed6e803cd9bce13da54c49abb0c6866da
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+
+    def save(self):
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        obj_dict = dict(self.__dict__)
+        obj_dict['__class__'] = self.__class__.__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+        return obj_dict
+
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,32 +1,35 @@
 import json
+from os import path
 
 class FileStorage:
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
-        return FileStorage.__objects
+        return self.__objects
 
     def new(self, obj):
-        key = f"{obj.__class__.__name__}.{obj.id}"
-        FileStorage.__objects[key] = obj
+        key = "{}.{}".format(obj.__class__.____name__, obj.id)
+        self.__objects[key] = obj
 
     def save(self):
-        data = {}
-        for key, obj in FileStorage.__objects.items():
-            data[key] = obj.to_dict()
-        with open(FileStorage.__file_path, 'w') as file:
-            json.dump(data, file)
+        obj_dict = {}
+        for key, obj in self.__objects.items():
+            obj_dict[key] = obj.to_dict()
+        
+        with open(self.__file_path, "w") as file:
+            json.dump(obj_dict, file)
 
     def reload(self):
-        try:
-            with open(FileStorage.__file_path, 'r') as file:
-                data = json.load(file)
-                for key, value in data.items():
-                    cls_name, obj_id = key.split('.')
-                    cls = models.classes.get(cls_name)
-                    instance = cls(**value)
-                    FileStorage.__objects[key] = instance
-        except FileNotFoundError:
-            pass
+        if path.exists(self.__file_path):
+            with open(self.__file_path, "r") as file:
+                obj_dict = json.load(file)
+                for key, obj in obj_dict.items():
+                    class_name, obj_id = key.split(".")
+                    cls = globals()[class_name]
+                    self.__objects[key] = cls(**obj)
+
+# Create a unique FileStorage instance
+storage = FileStorage()
+storage.reload()
 
